b4f24c51a0e30ab3fedd6db67479709837cec4c5
DaviDisDev <david.pr.dev@gmail.com>
2024-05-15 10:37:44 +0200

    fix routes

---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CodigoMunicipioController;
+
+Route::get('/', [CodigoMunicipioController::class, 'index']);
+
+Route::get('/getComunidades', [CodigoMunicipioController::class, 'getComunidades']);
+
+Route::get('/municipio/{id}', [CodigoMunicipioController::class, 'getMunicipio']);
+
+Route::get('/autocomplete/{string}', [CodigoMunicipioController::class, 'autocomplete']);
--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,3 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [CodigoMunicipioController::class, 'index']);
 
-Route::get('/getComunidades', [CodigoMunicipioController::class, 'getComunidades']);
-
-Route::get('/municipio/{id}', [CodigoMunicipioController::class, 'getMunicipio']);
-
-Route::get('/autocomplete/{string}', [CodigoMunicipioController::class, 'autocomplete']);
